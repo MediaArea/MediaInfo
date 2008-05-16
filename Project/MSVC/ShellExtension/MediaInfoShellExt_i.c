df77c3def72042e29e695b6ae84d6c93a86b8fe1
JeromeMartinez <jerome@mediaarea.net>
2008-05-16 13:59:07 +0000

    0.7.7.0
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@552 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Project/MSVC/ShellExtension/MediaInfoShellExt_i.c
+++ Project/MSVC/ShellExtension/MediaInfoShellExt_i.c
@@ -6,11 +6,11 @@
 
 
  /* File created by MIDL compiler version 6.00.0366 */
-/* at Sun Apr 27 12:35:47 2008
+/* at Fri May 16 15:34:53 2008
  */
 /* Compiler settings for .\MediaInfoShellExt.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
-    protocol : dce , ms_ext, c_ext
+    Oicf, W1, Zp8, env=Win64 (32b run)
+    protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
