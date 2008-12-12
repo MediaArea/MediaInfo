51811c158cf5186dca82376e8773229d051d914a
JeromeMartinez <jerome@mediaarea.net>
2008-12-12 11:05:11 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@1148 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Video/File_Vc1.cpp
+++ Source/MediaInfo/Video/File_Vc1.cpp
@@ -346,7 +346,13 @@ bool File_Vc1::Header_Parse_Fill_Size()
         Buffer_Offset_Temp=Buffer_Offset+4;
     while (Buffer_Offset_Temp+4<=Buffer_Size
         && CC3(Buffer+Buffer_Offset_Temp)!=0x000001)
-        Buffer_Offset_Temp++;
+    {
+        Buffer_Offset_Temp+=2;
+        while(Buffer_Offset_Temp<Buffer_Size && Buffer[Buffer_Offset_Temp]!=0x00)
+            Buffer_Offset_Temp+=2;
+        if (Buffer[Buffer_Offset_Temp-1]==0x00)
+            Buffer_Offset_Temp--;
+    }
 
     //Must wait more data?
     if (Buffer_Offset_Temp+4>Buffer_Size)
@@ -843,7 +849,13 @@ bool File_Vc1::Synchronize()
     //Synchronizing
     while (Buffer_Offset+4<=Buffer_Size
         && CC3(Buffer+Buffer_Offset)!=0x000001)
-        Buffer_Offset++;
+    {
+        Buffer_Offset+=2;
+        while(Buffer_Offset<Buffer_Size && Buffer[Buffer_Offset]!=0x00)
+            Buffer_Offset+=2;
+        if (Buffer_Offset<Buffer_Size && Buffer[Buffer_Offset-1]==0x00)
+            Buffer_Offset--;
+    }
     if (Buffer_Offset+4>Buffer_Size)
     {
         //Parsing last bytes
@@ -928,7 +940,13 @@ bool File_Vc1::Header_Parser_QuickSearch()
         //Getting size
         Buffer_Offset+=4;
         while(Buffer_Offset+4<=Buffer_Size && CC3(Buffer+Buffer_Offset)!=0x000001)
-            Buffer_Offset++;
+        {
+            Buffer_Offset+=2;
+            while(Buffer_Offset<Buffer_Size && Buffer[Buffer_Offset]!=0x00)
+                Buffer_Offset+=2;
+            if (Buffer_Offset<Buffer_Size && Buffer[Buffer_Offset-1]==0x00)
+                Buffer_Offset--;
+        }
     }
 
     if (Buffer_Offset+4<=Buffer_Size)
