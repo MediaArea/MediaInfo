9234f17d4c45783660bb9e28354f4d25333539a8
JeromeMartinez <jerome@mediaarea.net>
2009-04-07 10:31:04 +0000

    + Matroska: MLP detection
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@1651 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/MediaInfo_Config_Automatic.cpp
+++ Source/MediaInfo/MediaInfo_Config_Automatic.cpp
@@ -1937,6 +1937,7 @@ void MediaInfo_Config_CodecID_Audio_Matroska (InfoMap &Info)
     "A_REAL/RALF;RealAudio Lossless;;Real Audio Lossless Format;http://www.real.com\n"
     "A_REAL/ATRC;Atrac;;Real & Sony Atrac3 Codec;http://www.real.com\n"
     "A_TRUEHD;TrueHD;;Dolby TrueHD;http://www.dolby.com/consumer/technology/trueHD.html\n"
+    "A_MLP;MLP;;Meridian Lossless Packing;http://www.meridian-audio.com\n"
     "A_AAC;AAC\n"
     "A_AAC/MPEG2/MAIN;AAC\n"
     "A_AAC/MPEG2/LC;AAC\n"
