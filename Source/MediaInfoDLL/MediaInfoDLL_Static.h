087eec05669257a8c089df16ac1b77de85b1e838
JeromeMartinez <jerome@mediaarea.net>
2009-06-16 16:13:26 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@1910 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfoDLL/MediaInfoDLL_Static.h
+++ Source/MediaInfoDLL/MediaInfoDLL_Static.h
@@ -26,6 +26,107 @@
 #ifndef MediaInfoDLL_StaticH
 #define MediaInfoDLL_StaticH
 
+//***************************************************************************
+// Platforms (from libzen)
+//***************************************************************************
+
+/*---------------------------------------------------------------------------*/
+/*Win32*/
+#if defined(__NT__) || defined(_WIN32) || defined(WIN32)
+    #ifndef WIN32
+        #define WIN32
+    #endif
+    #ifndef _WIN32
+        #define _WIN32
+    #endif
+    #ifndef __WIN32__
+        #define __WIN32__ 1
+    #endif
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*Win64*/
+#if defined(_WIN64) || defined(WIN64)
+    #ifndef WIN64
+        #define WIN64
+    #endif
+    #ifndef _WIN64
+        #define _WIN64
+    #endif
+    #ifndef __WIN64__
+        #define __WIN64__ 1
+    #endif
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*Windows*/
+#if defined(WIN32) || defined(WIN64)
+    #ifndef WINDOWS
+        #define WINDOWS
+    #endif
+    #ifndef _WINDOWS
+        #define _WINDOWS
+    #endif
+    #ifndef __WINDOWS__
+        #define __WINDOWS__ 1
+    #endif
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*Unix (Linux, HP, Sun, BeOS...)*/
+#if defined(UNIX) || defined(_UNIX) || defined(__UNIX__) \
+    || defined(__unix) || defined(__unix__) \
+    || defined(____SVR4____) || defined(__LINUX__) || defined(__sgi) \
+    || defined(__hpux) || defined(sun) || defined(__SUN__) || defined(_AIX) \
+    || defined(__EMX__) || defined(__VMS) || defined(__BEOS__)
+    #ifndef UNIX
+        #define UNIX
+    #endif
+    #ifndef _UNIX
+        #define _UNIX
+    #endif
+    #ifndef __UNIX__
+        #define __UNIX__ 1
+    #endif
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*MacOS Classic*/
+#if defined(macintosh)
+    #ifndef MACOS
+        #define MACOS
+    #endif
+    #ifndef _MACOS
+        #define _MACOS
+    #endif
+    #ifndef __MACOS__
+        #define __MACOS__ 1
+    #endif
+#endif
+
+/*---------------------------------------------------------------------------*/
+/*MacOS X*/
+#if defined(__APPLE__) && defined(__MACH__)
+    #ifndef MACOSX
+        #define MACOSX
+    #endif
+    #ifndef _MACOSX
+        #define _MACOSX
+    #endif
+    #ifndef __MACOSX__
+        #define __MACOSX__ 1
+    #endif
+#endif
+
+/*Test of targets*/
+#if defined(WINDOWS) && defined(UNIX) && defined(MACOS) && defined(MACOSX)
+    #pragma message Multiple platforms???
+#endif
+
+#if !defined(WIN32) && !defined(UNIX) && !defined(MACOS) && !defined(MACOSX)
+    #pragma message No known platforms, assume default
+#endif
+
 /*-------------------------------------------------------------------------*/
 #if defined(_WIN32) && !defined(__MINGW32__) //MinGW32 does not support _declspec
     #ifdef MEDIAINFO_DLL_EXPORT
