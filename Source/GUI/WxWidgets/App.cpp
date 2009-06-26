da9909d972e6d6d0fec15fa57afdeff604aba61f
JeromeMartinez <jerome@mediaarea.net>
2009-06-26 10:46:42 +0000

    x #2786634, Mac OS X: files with non-US characters were not opened
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@1977 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/GUI/WxWidgets/App.cpp
+++ Source/GUI/WxWidgets/App.cpp
@@ -9,6 +9,10 @@
 #include "GUI/WxWidgets/App.h"
 #include "GUI/WxWidgets/GUI_Main.h"
 
+#ifdef __WXMAC__
+    #include <ApplicationServices/ApplicationServices.h>
+#endif //__WXMAC__
+
 // ----------------------------------------------------------------------------
 IMPLEMENT_APP(App)
 // ----------------------------------------------------------------------------
@@ -16,6 +20,14 @@ IMPLEMENT_APP(App)
 // ----------------------------------------------------------------------------
 bool App::OnInit()
 {
+    #ifdef __WXMAC__
+        ProcessSerialNumber PSN;
+        GetCurrentProcess(&PSN);
+        TransformProcessType(&PSN, kProcessTransformToForegroundApplication);
+    #endif //__WXMAC__
+
+    setlocale(LC_ALL, "");
+
     GUI_Main *frame = new GUI_Main(argc, argv, wxPoint(50, 50), wxSize(800-100, 600-100));
     frame->Show(TRUE);
     return TRUE;
