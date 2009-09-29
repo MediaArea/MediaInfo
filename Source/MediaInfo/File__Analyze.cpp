435216248286502e36557f99dbca827877548fd8
JeromeMartinez <jerome@mediaarea.net>
2009-09-29 10:36:38 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@2362 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/File__Analyze.cpp
+++ Source/MediaInfo/File__Analyze.cpp
@@ -294,7 +294,7 @@ void File__Analyze::Open_Buffer_Continue (const int8u* ToAdd, size_t ToAdd_Size)
     {
         if (Buffer_Offset>Buffer_Size)
             File_GoTo=File_Offset+Buffer_Offset;
-        if (File_Offset!=(int64u)-1)
+        if (!Status[IsFinished])
             File_Offset+=Buffer_Offset;
         Buffer_Clear();
         return;
