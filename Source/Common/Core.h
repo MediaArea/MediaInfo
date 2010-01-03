5890a06957d6895fb5282c7059627bf79a39750c
JeromeMartinez <jerome@mediaarea.net>
2010-01-03 17:23:17 +0000

    + Copyright changing
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@2654 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/Common/Core.h
+++ Source/Common/Core.h
@@ -1,5 +1,5 @@
 // Core - Main functions
-// Copyright (C) 2002-2009 Jerome Martinez, Zen@MediaArea.net
+// Copyright (C) 2002-2010 MediaArea.net SARL, Info@MediaArea.net
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
