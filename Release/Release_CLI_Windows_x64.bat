4875d874d9f9859c6fea2cb7d2f84a93192fc3d7
JeromeMartinez <jerome@mediaarea.net>
2010-05-02 05:38:07 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@3069 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Release/Release_CLI_Windows_x64.bat
+++ Release/Release_CLI_Windows_x64.bat
@@ -7,7 +7,7 @@ mkdir MediaInfo_CLI_Windows_x64
 
 rem --- Copying : Exe ---
 xcopy ..\Project\MSVC2010\CLI\x64\Release\MediaInfo.exe MediaInfo_CLI_Windows_x64\ /S
-xcopy ..\..\MediaInfoLib\Project\MSVC\DLL\x64\Release\MediaInfo.dll MediaInfo_CLI_Windows_x64\ /S
+xcopy ..\..\MediaInfoLib\Project\MSVC2010\DLL\x64\Release\MediaInfo.dll MediaInfo_CLI_Windows_x64\ /S
 
 rem --- Copying : Plugins ---
 xcopy ..\Source\Ressource\Plugin\Custom\* MediaInfo_CLI_Windows_x64\Plugin\Custom\ /S
