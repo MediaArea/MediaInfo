a5cec81941115408b322dfecaa7622037bdc261c
JeromeMartinez <jerome@mediaarea.net>
2010-08-17 21:10:46 +0000

    C#: FileOption_Recursive --> FileOption_NoRecursive
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@3249 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Project/MSCS/asp_net_web_application/MediaInfoDLL.cs
+++ Project/MSCS/asp_net_web_application/MediaInfoDLL.cs
@@ -77,7 +77,7 @@ namespace MediaInfoLib
     public enum InfoFileOptions
     {
         FileOption_Nothing      = 0x00,
-        FileOption_Recursive    = 0x01,
+        FileOption_NoRecursive  = 0x01,
         FileOption_CloseAll     = 0x02,
         FileOption_Max          = 0x04
     };
