42bff0bcb7f6adc6f45511f184b7e29aff10fd80
JeromeMartinez <jerome@mediaarea.net>
2010-09-05 16:28:23 +0000

    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@3296 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/GUI/VCL/GUI_About.cpp
+++ Source/GUI/VCL/GUI_About.cpp
@@ -38,7 +38,7 @@ TAboutF *AboutF;
 //---------------------------------------------------------------------------
 
 //---------------------------------------------------------------------------
-const ZenLib::Char* MEDIAINFO_ABOUT=     _T("MediaInfo X.X.X.X\\r\\nCopyright (C) 2002-2009 Jerome Martinez");
+const ZenLib::Char* MEDIAINFO_ABOUT=     _T("MediaInfo X.X.X.X\\r\\nCopyright (C) 2002-2010 MediaArea.net SARL");
 const ZenLib::Char* MEDIAINFO_URL=       _T("http://mediainfo.sourceforge.net");
 const ZenLib::Char* MEDIAINFO_NEWVERSION=_T("http://sourceforge.net/project/showfiles.php?group_id=86862&package_id=90341");
 const ZenLib::Char* MEDIAINFO_DONATE=    _T("http://mediainfo.sourceforge.net/Donate");
