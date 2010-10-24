3ae08ca792b910669306ded266ae501bafc6205f
JeromeMartinez <jerome@mediaarea.net>
2010-10-24 18:52:33 +0000

    Preparation of the official release
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@3404 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Project/MSVC2010/ShellExtension/MediaInfoShellExt_p.c
+++ Project/MSVC2010/ShellExtension/MediaInfoShellExt_p.c
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Wed Oct 13 23:13:39 2010
+/* at Sun Oct 24 20:50:41 2010
  */
 /* Compiler settings for MediaInfoShellExt.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
