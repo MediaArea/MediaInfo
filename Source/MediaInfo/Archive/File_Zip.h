16c6f284a7048b448aeeaa13b11190b281bc7d6f
JeromeMartinez <jerome@mediaarea.net>
2011-01-22 19:00:37 +0000

    + ZIP: parsing of file names
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@3710 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Archive/File_Zip.h
+++ Source/MediaInfo/Archive/File_Zip.h
@@ -42,9 +42,27 @@ class File_Zip : public File__Analyze
 protected :
     //Buffer - File header
     bool FileHeader_Begin();
+    
 
     //Buffer - Global
     void Read_Buffer_Continue ();
+    
+    int32u compressed_size;
+    bool data_descriptor_set;
+    int32u signature;
+    int8u  local_file_Step;
+    bool    end_of_central_directory_IsParsed;
+
+    bool local_file();
+    bool local_file_header();
+    bool file_data();
+    bool data_descriptor();
+    bool archive_extra_data_record();
+    bool central_directory();
+    bool digital_signature();
+    bool end_of_central_directory();
+    bool Zip64_end_of_central_directory_record();
+    bool Zip64_end_of_central_directory_locator();
 };
 
 } //NameSpace
