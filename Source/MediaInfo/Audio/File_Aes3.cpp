359f7a48671cc5dedee733eb9aed1e60c65214f7
JeromeMartinez <jerome@mediaarea.net>
2011-02-08 22:49:46 +0000

    x AES3 in MPEG-TS: Endianess was set with random value (correction)
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@3777 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Audio/File_Aes3.cpp
+++ Source/MediaInfo/Audio/File_Aes3.cpp
@@ -1091,7 +1091,7 @@ void File_Aes3::Frame_FromMpegPs()
     {
         Accept("AES3");
         Container_Bits=Stream_Bits=16+4*bits_per_sample;
-        Endianess=false; //Big
+        Endianess=true; //Little
         if (Container_Bits%8)
             Container_Bits+=4; //Rounded to next byte
     }
