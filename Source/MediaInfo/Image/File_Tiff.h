99824cc0afb2d3c36bd05a62baddfc363b1a5e30
JeromeMartinez <jerome@mediaarea.net>
2011-04-19 14:27:53 +0000

    + TIFF: more details
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@3950 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Image/File_Tiff.h
+++ Source/MediaInfo/Image/File_Tiff.h
@@ -38,13 +38,42 @@ namespace MediaInfoLib
 //***************************************************************************
 
 class File_Tiff : public File__Analyze
-{
+{ 
+public:
+    //Constructor/Destructor
+    File_Tiff();
+
 private :
     //Buffer - File header
     bool FileHeader_Begin();
+    void FileHeader_Parse();
 
-    //Buffer - Global
-    void Read_Buffer_Continue ();
+    //Buffer - Per element
+    void Header_Parse();
+    void Data_Parse();
+    void Data_Parse_Fill();
+
+	//Elements
+    void Read_Directories();
+	void Read_Directory();
+		
+    //Temp
+    struct ifditem
+    {
+        int16u Tag;
+        int16u Type;
+        int32u Count;
+    };
+    typedef std::map<int32u, ifditem> ifditems; //Key is byte offset
+    ifditems IfdItems;
+    typedef std::map<int16u, ZtringList> infos; //Key is Tag value
+    infos Infos;
+	bool LittleEndian;
+
+    //Helpers
+    void Get_X2 (int16u &Info, const char* Name);
+    void Get_X4 (int32u &Info, const char* Name);
+	void GetValueOffsetu(ifditem &IfdItem);
 };
 
 } //NameSpace
