79d191a8209c9531bf94b90255301d34d0f2c4eb
JeromeMartinez <jerome@mediaarea.net>
2011-07-31 11:14:47 +0000

    + Time code class helper
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4201 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

---
+++ Source/MediaInfo/TimeCode.cpp
@@ -0,0 +1,125 @@
+// TimeCode - Time code operations with drop frame support
+// Copyright (C) 2011-2011 MediaArea.net SARL, Info@MediaArea.net
+//
+// This library is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//---------------------------------------------------------------------------
+// Compilation conditions
+#include "MediaInfo/Setup.h"
+#ifdef __BORLANDC__
+    #pragma hdrstop
+#endif
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+#include "MediaInfo/TimeCode.h"
+//---------------------------------------------------------------------------
+
+namespace MediaInfoLib
+{
+
+//***************************************************************************
+// Constructor/Destructor
+//***************************************************************************
+
+//---------------------------------------------------------------------------
+TimeCode::TimeCode ()
+{
+    Hours=0;
+    Minutes=0;
+    Seconds=0;
+    Frames=0;
+    FramesPerSecond=0;
+    DropFrame=0;
+}
+
+//---------------------------------------------------------------------------
+TimeCode::TimeCode (int8u Hours_, int8u Minutes_, int8u Seconds_, int8u Frames_, int8u FramesPerSecond_, bool DropFrame_)
+{
+    Hours=Hours_;
+    Minutes=Minutes_;
+    Seconds=Seconds_;
+    Frames=Frames_;
+    FramesPerSecond=FramesPerSecond_;
+    DropFrame=DropFrame_;
+}
+
+//***************************************************************************
+// Helpers
+//***************************************************************************
+
+//---------------------------------------------------------------------------
+void TimeCode::PlusOne()
+{
+    if (FramesPerSecond==0)
+        return;
+    Frames++;
+    if (Frames>=FramesPerSecond)
+    {
+        Seconds++;
+        Frames=0;
+        if (Seconds>=60)
+        {
+            Seconds=0;
+            Minutes++;
+
+            if (DropFrame && Minutes%10)
+                Frames=2; //frames 0 and 1 are dropped for every minutes except 00 10 20 30 40 50
+
+            if (Minutes>=60)
+            {
+                Minutes=0;
+                Hours++;
+                if (Hours>=24)
+                {
+                    Hours=0;
+                }
+            }
+        }
+    }
+}
+
+//---------------------------------------------------------------------------
+void TimeCode::MinusOne()
+{
+    if (FramesPerSecond==0)
+        return;
+    if (Frames==0 || (DropFrame && Minutes%10 && Frames<=2))
+    {
+        Frames=FramesPerSecond;
+        if (Seconds==0)
+        {
+            Seconds=60;
+            if (Minutes==0)
+            {
+                Minutes=60;
+                if (Hours==0)
+                    Hours=24;
+                Hours--;
+            }
+            Minutes--;
+        }
+        Seconds--;
+    }
+    Frames--;
+}
+
+//***************************************************************************
+//
+//***************************************************************************
+
+} //NameSpace
