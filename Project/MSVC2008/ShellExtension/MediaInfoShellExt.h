21ee722370c59db4bea14b86c47fa6ea1e975272
JeromeMartinez <jerome@mediaarea.net>
2011-09-10 17:57:43 +0000

    Preparation of new release
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4290 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Project/MSVC2008/ShellExtension/MediaInfoShellExt.h
+++ Project/MSVC2008/ShellExtension/MediaInfoShellExt.h
@@ -4,11 +4,11 @@
 
 
  /* File created by MIDL compiler version 7.00.0500 */
-/* at Wed Jun 15 11:56:44 2011
+/* at Sat Sep 10 19:43:56 2011
  */
 /* Compiler settings for .\MediaInfoShellExt.idl:
-    Oicf, W1, Zp8, env=Win32 (32b run)
-    protocol : dce , ms_ext, c_ext
+    Oicf, W1, Zp8, env=Win64 (32b run)
+    protocol : dce , ms_ext, c_ext, robust
     error checks: allocation ref bounds_check enum stub_data 
     VC __declspec() decoration level: 
          __declspec(uuid()), __declspec(selectany), __declspec(novtable)
@@ -21,7 +21,7 @@
 
 /* verify that the <rpcndr.h> version is high enough to compile this file*/
 #ifndef __REQUIRED_RPCNDR_H_VERSION__
-#define __REQUIRED_RPCNDR_H_VERSION__ 440
+#define __REQUIRED_RPCNDR_H_VERSION__ 475
 #endif
 
 #include "rpc.h"
--- Project/MSVC2010/ShellExtension/MediaInfoShellExt.h
+++ Project/MSVC2010/ShellExtension/MediaInfoShellExt.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Tue Jul 05 17:52:26 2011
+/* at Sat Sep 10 19:45:33 2011
  */
 /* Compiler settings for MediaInfoShellExt.idl:
     Oicf, W1, Zp8, env=Win64 (32b run), target_arch=AMD64 7.00.0555 
