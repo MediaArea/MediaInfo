40ca8594c851c653b4146893bec5a77eaebd485c
JeromeMartinez <jerome@mediaarea.net>
2011-09-23 09:19:59 +0000

    Preparation of new version
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4332 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/MediaInfo_Config.cpp
+++ Source/MediaInfo/MediaInfo_Config.cpp
@@ -46,7 +46,7 @@ namespace MediaInfoLib
 {
 
 //---------------------------------------------------------------------------
-const Char*  MediaInfo_Version=_T("MediaInfoLib - v0.7.49");
+const Char*  MediaInfo_Version=_T("MediaInfoLib - v0.7.50");
 const Char*  MediaInfo_Url=_T("http://mediainfo.sourceforge.net");
       Ztring EmptyZtring;       //Use it when we can't return a reference to a true Ztring
 const Ztring EmptyZtring_Const; //Use it when we can't return a reference to a true Ztring, const version
