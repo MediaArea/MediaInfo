426ee428c2a6fb5ec065199aa2010ccaa12e2ec3
JeromeMartinez <jerome@mediaarea.net>
2012-02-24 22:41:37 +0000

    + MPEG-2 Video macroblocks parsing (update)
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4670 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Video/File_Mpegv.cpp
+++ Source/MediaInfo/Video/File_Mpegv.cpp
@@ -2269,29 +2269,12 @@ void File_Mpegv::slice_start()
                     Remain=Buffer[Buffer_Offset+(size_t)(Element_Size-Data_Remain())];
                 if (Remain==0)
                 {
-                    size_t BytesRemain=Data_Remain();
-                    if (BytesRemain>1)
-                    {
-                        if (Buffer[Buffer_Offset+(size_t)(Element_Size-BytesRemain)+1]==0x00)
-                        {
-                            if (BytesRemain>2)
-                            {
-                                if (Buffer[Buffer_Offset+(size_t)(Element_Size-BytesRemain)+2]==0x00)
-                                {
-                                    if (BytesRemain>3)
-                                    {
-                                        if (Buffer[Buffer_Offset+(size_t)(Element_Size-BytesRemain)+3]==0x00)
-                                            break;
-                                    }
-                                    else
-                                        break;
-                                }
-                            }
-                            else
-                                break;
-                        }
-                    }
-                    else
+                    int32u Remain3;
+                    size_t Bits=Data_BS_Remain();
+                    if (Bits>23)
+                        Bits=23;
+                    Peek_S3((int8u)Bits, Remain3);
+                    if (Remain3==0)
                         break;
                 }
                 slice_start_macroblock();
@@ -2585,7 +2568,6 @@ void File_Mpegv::slice_start_macroblock()
         {
             BS_End();
             Element_Offset=Element_Size;
-            Finish();
             return;
         }
         Element_Info1(_T("macroblock_quant=")+Ztring::ToZtring(macroblock_type&macroblock_quant));
