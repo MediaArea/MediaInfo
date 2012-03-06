25086e19bc65fc25ac578d784b81fdfdd581d7a2
JeromeMartinez <jerome@mediaarea.net>
2012-03-06 08:03:18 +0000

    x Removing a warning
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4684 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/File__Analyze_Streams_Finish.cpp
+++ Source/MediaInfo/File__Analyze_Streams_Finish.cpp
@@ -534,7 +534,7 @@ void File__Analyze::Streams_Finish_InterStreams()
         bool VideobitRateIsValid=true;
         for (size_t Pos=0; Pos<Count_Get(Stream_Audio); Pos++)
         {
-            float64 AudioBitRate;
+            float64 AudioBitRate=0;
             if (!Retrieve(Stream_Audio, Pos, Audio_BitRate).empty() && Retrieve(Stream_Audio, Pos, Audio_BitRate)[0]<=_T('9')) //Note: quick test if it is a number
                 AudioBitRate=Retrieve(Stream_Audio, Pos, Audio_BitRate).To_float64();
             else if (!Retrieve(Stream_Audio, Pos, Audio_BitRate_Encoded).empty() && Retrieve(Stream_Audio, Pos, Audio_BitRate_Encoded)[0]<=_T('9')) //Note: quick test if it is a number        
