574281e3b6e763c220f24482d21363b642eed3c8
JeromeMartinez <jerome@mediaarea.net>
2012-07-02 17:48:23 +0000

    x MPEG-PS: crash if CA_PID is not understood
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@4903 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Multiple/File_Mpeg_Descriptors.cpp
+++ Source/MediaInfo/Multiple/File_Mpeg_Descriptors.cpp
@@ -1662,7 +1662,7 @@ void File_Mpeg_Descriptors::Descriptor_09()
             case 0x02 : //program_map_section
                         if (elementary_PID_IsValid)
                         {
-                            if (Complete_Stream->Streams[CA_PID]->Kind==complete_stream::stream::unknown) //Priority to PES, if this is a PES, we skip the CA
+                            if (CA_PID<Complete_Stream->Streams.size() && Complete_Stream->Streams[CA_PID]->Kind==complete_stream::stream::unknown) //Priority to PES, if this is a PES, we skip the CA
                             {
                                 Complete_Stream->Streams[CA_PID]->Kind=complete_stream::stream::psi;
                                 Complete_Stream->Streams[CA_PID]->Table_IDs.resize(0x100);
