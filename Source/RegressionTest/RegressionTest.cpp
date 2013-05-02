086781f5de190f9674d8a488e8ef1e02448ed2f6
JeromeMartinez <jerome@mediaarea.net>
2013-05-02 12:33:38 +0000

    + MD5 computing test
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@5498 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/RegressionTest/RegressionTest.cpp
+++ Source/RegressionTest/RegressionTest.cpp
@@ -11,18 +11,50 @@ using namespace ZenLib;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-    if (argc!=4)
+    if (argc<4)
         return 1;
 
     Ztring Files=argv[1];
     Ztring DataBaseDirectory=argv[2];
     int32u Scenario=Ztring(argv[3]).To_int32u();
 
-    cout<<"Basic"<<endl;
-    RegressionTest_Basic(Files, DataBaseDirectory, Scenario);
+    bool Flag_All=true, Flag_Basic=false, Flag_Events=false, Flag_Md5=false;
+    for (int Pos=3; Pos<argc; Pos++)
+    {
+        if (Ztring(argv[Pos]).Compare(__T("Basic")))
+        {
+            Flag_All=false;
+            Flag_Basic=true;
+        }
+        if (Ztring(argv[Pos]).Compare(__T("Events")))
+        {
+            Flag_All=false;
+            Flag_Events=true;
+        }
+        if (Ztring(argv[Pos]).Compare(__T("Md5")))
+        {
+            Flag_All=false;
+            Flag_Md5=true;
+        }
+    }
 
-    //cout<<"Events"<<endl;
-    //RegressionTest_Events(Files, DataBaseDirectory, Scenario);
+    if (Flag_All || Flag_Basic)
+    {
+        cout<<"Basic"<<endl;
+        RegressionTest_Basic(Files, DataBaseDirectory, Scenario);
+    }
+
+    if (Flag_All || Flag_Events)
+    {
+        cout<<"Events"<<endl;
+        RegressionTest_Events(Files, DataBaseDirectory, Scenario);
+    }
+
+    if (Flag_All || Flag_Md5)
+    {
+        cout<<"Md5"<<endl;
+        RegressionTest_Md5(Files, DataBaseDirectory, Scenario);
+    }
 
     return 0;
 }
--- Source/RegressionTest/RegressionTest.h
+++ Source/RegressionTest/RegressionTest.h
@@ -3,3 +3,4 @@ using namespace ZenLib;
 
 void RegressionTest_Basic(Ztring Files, Ztring DataBaseDirectory, int32u Scenario);
 void RegressionTest_Events(Ztring Files, Ztring DataBaseDirectory, int32u Scenario);
+void RegressionTest_Md5(Ztring Files, Ztring DataBaseDirectory, int32u Scenario);
