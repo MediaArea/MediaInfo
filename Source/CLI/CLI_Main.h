fdb0459e0261c85ad3e7ff93a665c92cfdcaa05a
JeromeMartinez <jerome@mediaarea.net>
2013-05-12 12:42:07 +0000

    + Switched to a BSD-style license
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@5555 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/CLI/CLI_Main.h
+++ Source/CLI/CLI_Main.h
@@ -1,23 +1,9 @@
-// MediaInfo_CLI - A Command Line Interface for MediaInfoLib
-// Copyright (C) 2002-2012 MediaArea.net SARL, Info@MediaArea.net
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Library General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-//
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license that can
+ *  be found in the License.html file in the root of the source tree.
+ */
 
-//---------------------------------------------------------------------------
 #ifndef CLI_MainH
 #define CLI_MainH
 //---------------------------------------------------------------------------
--- Source/GUI/VCL/GUI_Preferences_Output.h
+++ Source/GUI/VCL/GUI_Preferences_Output.h
@@ -1,36 +1,10 @@
-// IPreferences_Output - Preferences interface of MediaInfo
-// Copyright (C) 2002-2012 MediaArea.net SARL, Info@MediaArea.net
-//
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Library General Public License as published by
-// the Free Software Foundation, either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Library General Public License for more details.
-//
-// You should have received a copy of the GNU Library General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-//
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//
-// Version 0.4.0
-// -------------
-// Preferences interface of MediaInfo (Output definition part)
-//
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//
-// 0.4.0 (Zen@MediaArea.net, 2003-07-21)
-// Adapt to new architecture and being GPL
-//
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license that can
+ *  be found in the License.html file in the root of the source tree.
+ */
 
 //---------------------------------------------------------------------------
-
 #ifndef GUI_Preferences_OutputH
 #define GUI_Preferences_OutputH
 //---------------------------------------------------------------------------
