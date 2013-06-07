96aa469fdaeddc05ce092ba926c1cf799d691bfc
JeromeMartinez <jerome@mediaarea.net>
2013-06-07 09:03:47 +0000

    + MD5 test in a standalone function
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@5634 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/RegressionTest/RegressionTest_Md5.cpp
+++ Source/RegressionTest/RegressionTest_Md5.cpp
@@ -23,10 +23,71 @@ using namespace MediaInfoDLL;
 using namespace ZenLib;
 using namespace std;
 
-void RegressionTest_Md5(Ztring Files, Ztring DataBaseDirectory, int32u Scenario)
+void WriteMd5Info(const Ztring &FileName, MediaInfo &MI, const Ztring &DataBaseDirectory)
 {
     int8u* Buffer=new int8u[0x1000000];
+    File F;
+        
+    String MD5_FromMI=MI.Get(Stream_General, 0, __T("MD5"));
+    String Format=MI.Get(Stream_General, 0, __T("Format"));
 
+    Ztring MD5_FromFile;
+    if (!MD5_FromMI.empty())
+    {
+        F.Open(FileName);
+        MD5Context Context;
+        MD5Init(&Context);
+        size_t Size;
+        do
+        {
+            Size=F.Read(Buffer, 0x1000000);
+            MD5Update(&Context, Buffer, Size);
+        }
+        while (Size==0x1000000);
+        unsigned char Digest[16];
+        MD5Final(Digest, &Context);
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 0));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 2));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 4));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 6));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 8));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+10));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+12));
+        MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+14));
+        MD5_FromFile.MakeLowerCase();
+    }
+
+    bool EmptyNormal=false;
+    if (MD5_FromMI.empty() && Format==__T("MXF") && MI.Get(Stream_General, 0, __T("Format_Settings")).find(__T("Open / "))!=string::npos)
+    {
+        Format+=__T(" ")+MI.Get(Stream_General, 0, __T("Format_Settings"));
+        EmptyNormal=true;
+    }
+    if (MD5_FromMI.empty() && Format.empty())
+    {
+        EmptyNormal=true;
+    }
+    if (MD5_FromMI.empty() || EmptyNormal)
+    {
+        F.Open(DataBaseDirectory+(EmptyNormal?__T("\\MD5.EmptyNormal.csv"):__T("\\MD5.________________Missing.csv")), File::Access_Write_Append);
+        F.Write(FileName+__T(";")+MI.Get(Stream_General, 0, __T("Format"))+EOL);
+    }
+    else if (MD5_FromMI==MD5_FromFile)
+    {
+        F.Open(DataBaseDirectory+__T("\\MD5.Success.csv"), File::Access_Write_Append);
+        F.Write(FileName+__T(";")+Format+__T(";")+MD5_FromMI+EOL);
+    }
+    else
+    {
+        F.Open(DataBaseDirectory+__T("\\MD5.________________Fail.csv"), File::Access_Write_Append);
+        F.Write(FileName+__T(";")+MI.Get(Stream_General, 0, __T("Format"))+__T(";")+MD5_FromMI+__T(";")+MD5_FromFile+EOL);
+    }
+
+    delete[] Buffer;
+}
+
+void RegressionTest_Md5(Ztring Files, Ztring DataBaseDirectory, int32u Scenario)
+{
     cout<<" Analyzing"<<endl;
 
     ZtringList FilesList=Dir::GetAllFileNames(Files);
@@ -54,63 +115,10 @@ void RegressionTest_Md5(Ztring Files, Ztring DataBaseDirectory, int32u Scenario)
 
         MediaInfo MI;
         MI.Option(__T("ParseSpeed"), __T("1"));
+        MI.Option(__T("File_Source_List"), __T("1"));
         MI.Option(__T("File_MD5"), __T("1"));
         MI.Open(FilesList[FilesList_Pos]);
-        Ztring MD5_FromMI=MI.Get(Stream_General, 0, __T("MD5"));
 
-        Ztring MD5_FromFile;
-        if (!MD5_FromMI.empty())
-        {
-            F.Open(FilesList[FilesList_Pos]);
-            MD5Context Context;
-            MD5Init(&Context);
-            size_t Size;
-            do
-            {
-                Size=F.Read(Buffer, 0x1000000);
-                MD5Update(&Context, Buffer, Size);
-            }
-            while (Size==0x1000000);
-            unsigned char Digest[16];
-            MD5Final(Digest, &Context);
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 0));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 2));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 4));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 6));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+ 8));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+10));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+12));
-            MD5_FromFile+=Ztring().From_CC2(BigEndian2int16u(Digest+14));
-            MD5_FromFile.MakeLowerCase();
-        }
-
-        Ztring Format=MI.Get(Stream_General, 0, __T("Format"));
-        bool EmptyNormal=false;
-        if (MD5_FromMI.empty() && Format==__T("MXF") && MI.Get(Stream_General, 0, __T("Format_Settings")).find(__T("Open / "))!=string::npos)
-        {
-            Format+=__T(" ")+MI.Get(Stream_General, 0, __T("Format_Settings"));
-            EmptyNormal=true;
-        }
-        if (MD5_FromMI.empty() && Format.empty())
-        {
-            EmptyNormal=true;
-        }
-        if (MD5_FromMI.empty() || EmptyNormal)
-        {
-            F.Open(DataBaseDirectory+(EmptyNormal?__T("\\MD5.EmptyNormal.csv"):__T("\\MD5.________________Missing.csv")), File::Access_Write_Append);
-            F.Write(FilesList[FilesList_Pos]+__T(";")+MI.Get(Stream_General, 0, __T("Format"))+EOL);
-        }
-        else if (MD5_FromMI==MD5_FromFile)
-        {
-            F.Open(DataBaseDirectory+__T("\\MD5.Success.csv"), File::Access_Write_Append);
-            F.Write(FilesList[FilesList_Pos]+__T(";")+Format+__T(";")+MD5_FromMI+EOL);
-        }
-        else
-        {
-            F.Open(DataBaseDirectory+__T("\\MD5.________________Fail.csv"), File::Access_Write_Append);
-            F.Write(FilesList[FilesList_Pos]+__T(";")+MI.Get(Stream_General, 0, __T("Format"))+__T(";")+MD5_FromMI+__T(";")+MD5_FromFile+EOL);
-        }
+        WriteMd5Info(FilesList[FilesList_Pos], MI, DataBaseDirectory);
     }
-
-    delete[] Buffer;
 }
