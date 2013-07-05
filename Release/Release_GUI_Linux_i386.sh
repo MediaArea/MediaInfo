969ee5590247db03caafec95a1aa792110fe25b4
JeromeMartinez <jerome@mediaarea.net>
2013-07-05 09:15:53 +0000

    Typos
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@5791 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Release/Release_GUI_Linux_i386.sh
+++ Release/Release_GUI_Linux_i386.sh
@@ -3,7 +3,6 @@
  #  Use of this source code is governed by a BSD-style license that can
  #  be found in the License.html file in the root of the source tree.
  ##
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 #! /bin/sh
 
