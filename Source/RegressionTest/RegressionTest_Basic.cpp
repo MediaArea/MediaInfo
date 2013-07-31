25220f0ce5d99cc1e30e2fd3e67b02dcb36dec18
JeromeMartinez <jerome@mediaarea.net>
2013-07-31 22:07:56 +0000

    x Memory usage reduction
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@5851 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/RegressionTest/RegressionTest_Basic.cpp
+++ Source/RegressionTest/RegressionTest_Basic.cpp
@@ -12,6 +12,7 @@
 #include "MediaInfoDLL/MediaInfoDLL.h"
 #include "ZenLib/ZtringListListF.h"
 #include "ZenLib/File.h"
+#include "ZenLib/FileName.h"
 #include "ZenLib/Dir.h"
 #include "RegressionTest/RegressionTest.h"
 #include "MediaInfo/MediaInfo_Events.h"
@@ -121,6 +122,22 @@ void __stdcall Basic_Event_CallBackFunction(unsigned char* Data_Content, size_t 
 
 void RegressionTest_Basic(Ztring Files, Ztring DataBaseDirectory, int32u Scenario)
 {
+    /*
+    //One per one
+    ZtringList List=Dir::GetAllFileNames(Files);
+    for (size_t Pos=0; Pos<List.size(); Pos++)
+    {
+        std::cout<<List[Pos].To_Local()<<endl;
+        MediaInfoDLL_Load();
+        MediaInfoList* MIL=new MediaInfoList;
+        MIL->Open(List[Pos]);
+        delete MIL;
+        MediaInfoDLL_UnLoad();
+        File::Move(L"C:\\Temp\\Debug_MemoryLeak.txt", L"C:\\Temp\\"+ZenLib::FileName(List[Pos]).Name_Get()+L"."+ZenLib::FileName(List[Pos]).Extension_Get()+L"."+Ztring::ToZtring(Pos)+L".txt");
+    }
+    */
+
+    /* Old
     ZtringListListF* Ref=new ZtringListListF[Stream_Max];
     for (size_t StreamKind=0; StreamKind<Stream_Max; StreamKind++)
         if (File::Exists(DataBaseDirectory+__T("\\Basic\\Ref\\")+Ztring::ToZtring(StreamKind)+__T(".csv")))
@@ -246,6 +263,127 @@ void RegressionTest_Basic(Ztring Files, Ztring DataBaseDirectory, int32u Scenari
             if (!Dir::Exists(DataBaseDirectory+__T("\\Basic\\Diff")))
                 Dir::Create(DataBaseDirectory+__T("\\Basic\\Diff"));
             Diff[StreamKind].Save(DataBaseDirectory+__T("\\Basic\\Diff\\")+Ztring::ToZtring(StreamKind)+__T(".csv"));
+        }
+    }
+    */
+
+    MediaInfoList MIL;
+    struct UserHandle_struct UserHandle;
+
+    //Times
+    wostringstream Event_CallBackFunction_Text;
+    Event_CallBackFunction_Text<<__T("CallBack=memory://")<<(MediaInfo_int64u)Basic_Event_CallBackFunction<<__T(";UserHandler=memory://")<<(MediaInfo_int64u)&UserHandle;
+    MIL.Option(__T("File_Event_CallBackFunction"), Event_CallBackFunction_Text.str());
+    if (!Dir::Exists(DataBaseDirectory+__T("\\Basic\\Diff")))
+        Dir::Create(DataBaseDirectory+__T("\\Basic\\Diff"));
+    File::Delete(DataBaseDirectory+__T("\\Basic\\Diff\\Times.csv"));
+    UserHandle.Time_File.Open(DataBaseDirectory+__T("\\Basic\\Diff\\Times.csv"), File::Access_Write_Append);
+
+    cout<<" Analyzing"<<endl;
+    MIL.Open(Files);
+
+
+    for (size_t StreamKind=0; StreamKind<Stream_Max; StreamKind++)
+    {
+        ZtringListListF New;
+        ZtringListListF Ref;
+        ZtringListListF Diff;
+
+        cout<<" Retrieving ref data"<<endl;
+        if (File::Exists(DataBaseDirectory+__T("\\Basic\\Ref\\")+Ztring::ToZtring(StreamKind)+__T(".csv")))
+            Ref.Load(DataBaseDirectory+__T("\\Basic\\Ref\\")+Ztring::ToZtring(StreamKind)+__T(".csv"));
+
+        cout<<" Retrieving new data"<<endl;
+        for (size_t FilePos=0; FilePos<MIL.Count_Get(); FilePos++)
+            if (MIL.Count_Get(FilePos, (stream_t)StreamKind))
+            {
+                New.push_back(ZtringList());
+                New.at(New.size()-1).push_back(Ztring()); //CompleteFileName
+                for (size_t LinePos=0; LinePos<MIL.Count_Get(FilePos, (stream_t)StreamKind, 0); LinePos++)
+                    New.at(New.size()-1).push_back(MIL.Get(FilePos, (stream_t)StreamKind, 0, LinePos, Info_Name));
+                break;
+            }
+
+        for (size_t FilePos=0; FilePos<MIL.Count_Get(); FilePos++)
+            for (size_t StreamPos=0; StreamPos<MIL.Count_Get(FilePos, (stream_t)StreamKind); StreamPos++)
+            {
+                New.push_back(ZtringList());
+                New.at(New.size()-1).push_back(MIL.Get(FilePos, Stream_General, 0, __T("CompleteName")));
+                for (size_t LinePos=0; LinePos<MIL.Count_Get(FilePos, (stream_t)StreamKind, StreamPos); LinePos++)
+                    New.at(New.size()-1).push_back(MIL.Get(FilePos, (stream_t)StreamKind, StreamPos, LinePos));
+            }
+
+        if (!Dir::Exists(DataBaseDirectory+__T("\\Basic\\New")))
+            Dir::Create(DataBaseDirectory+__T("\\Basic\\New"));
+        New.Save(DataBaseDirectory+__T("\\Basic\\New\\")+Ztring::ToZtring(StreamKind)+__T(".csv"));
+
+        cout<<" Diff"<<endl;
+        size_t Ref_Pos=1;
+        size_t New_Pos=1;
+        while (Ref_Pos<Ref.size() && New_Pos<New.size())
+        {
+            if (New(New_Pos, 0)!=Ref(Ref_Pos, 0))
+            {
+                size_t Ref_PosNext=Ref_Pos;
+                size_t New_PosNext=New_Pos;
+                while (Ref_PosNext<Ref.size() && New(New_Pos, 0)!=Ref(Ref_PosNext, 0))
+                    Ref_PosNext++;
+                if (Ref_PosNext==Ref.size()) //Ref file not found
+                {
+                    Diff.push_back(New(New_Pos));
+                    New_Pos++;
+                }
+                else
+                {
+                    for (; Ref_Pos<Ref_PosNext; Ref_Pos++)
+                        Diff.push_back(Ref(Ref_Pos));
+                }
+            }
+            else
+            {
+                bool IsDiff=false;
+                size_t LinePos_Max=max(New(New_Pos).size(), Ref(Ref_Pos).size());
+                for (size_t LinePos=1; LinePos<LinePos_Max; LinePos++)
+                    if (New(New_Pos, LinePos)!=Ref(Ref_Pos, LinePos))
+                    {
+                        if (!IsDiff)
+                        {
+                            Diff.push_back(ZtringList());
+                            Diff(Diff.size()-1, 0)=New(New_Pos, 0);
+                            IsDiff=true;
+                        }
+                        Diff(Diff.size()-1, LinePos)=New(New_Pos, LinePos)+__T(" --- ")+Ref(Ref_Pos, LinePos);
+                    }
+
+                Ref_Pos++;
+                New_Pos++;
+            }
+
+        }
+
+        for (; Ref_Pos<Ref.size(); Ref_Pos++)
+        {
+            Diff.push_back(ZtringList());
+            Diff(Diff.size()-1, 0)=Ref(Ref_Pos, 0);
+            for (size_t LinePos=1; LinePos<Ref[Ref_Pos].size(); LinePos++)
+                if (!Ref(Ref_Pos, LinePos).empty())
+                    Diff(Diff.size()-1, LinePos)=__T(" --- ")+Ref(Ref_Pos, LinePos);
+        }
+        for (; New_Pos<New.size(); New_Pos++)
+        {
+            Diff.push_back(ZtringList());
+            Diff(Diff.size()-1, 0)=New(New_Pos, 0);
+            for (size_t LinePos=1; LinePos<New[New_Pos].size(); LinePos++)
+                if (!New(New_Pos, LinePos).empty())
+                    Diff(Diff.size()-1, LinePos)=New(New_Pos, LinePos)+__T(" --- ");
+        }
+
+        if (!Diff.empty())
+        {
+            Diff.insert(Diff.begin(), New(0));
+            if (!Dir::Exists(DataBaseDirectory+__T("\\Basic\\Diff")))
+                Dir::Create(DataBaseDirectory+__T("\\Basic\\Diff"));
+            Diff.Save(DataBaseDirectory+__T("\\Basic\\Diff\\")+Ztring::ToZtring(StreamKind)+__T(".csv"));
         }
     }
 }
