14aff6f3fd0796eec236734e33708e4014a937c5
JeromeMartinez <jerome@mediaarea.net>
2013-08-29 08:32:15 +0000

    x B786, MPEG-4 Visual: wrong PAR 40:3 (thanks to cyang)
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@5901 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Video/File_Mpeg4v.cpp
+++ Source/MediaInfo/Video/File_Mpeg4v.cpp
@@ -408,7 +408,7 @@ void File_Mpeg4v::Streams_Fill()
              if (aspect_ratio_info==0x02) PixelAspectRatio_Value=(float32)12/(float32)11;
         else if (aspect_ratio_info==0x03) PixelAspectRatio_Value=(float32)10/(float32)11;
         else if (aspect_ratio_info==0x04) PixelAspectRatio_Value=(float32)16/(float32)11;
-        else if (aspect_ratio_info==0x05) PixelAspectRatio_Value=(float32)40/(float32)13;
+        else if (aspect_ratio_info==0x05) PixelAspectRatio_Value=(float32)40/(float32)33;
         else if (aspect_ratio_info==0x0F && par_height) PixelAspectRatio_Value=((float32)par_width)/par_height;
         Fill(Stream_Video, 0, Video_PixelAspectRatio, PixelAspectRatio_Value, 3, true);
         Fill(Stream_Video, StreamPos_Last, Video_DisplayAspectRatio, ((float)object_layer_width)/object_layer_height*PixelAspectRatio_Value, 3, true);
