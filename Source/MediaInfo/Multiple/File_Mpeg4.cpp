6b6f766d7e2dba7936fd31af94472b95fb67986a
JeromeMartinez <jerome@mediaarea.net>
2013-12-16 21:12:34 +0000

    + QuickTime: new field ScanOrder_StoredDisplayedInverted, set to "Yes" when display and Stored orders are inverted x QuickTime: false-positive detection of incoherency between container and raw stream scan order due to inverted display and tored orders
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@6006 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Multiple/File_Mpeg4.cpp
+++ Source/MediaInfo/Multiple/File_Mpeg4.cpp
@@ -709,6 +709,16 @@ void File_Mpeg4::Streams_Finish()
             }
         }
 
+        //ScanOrder_StoredDisplayedInverted
+        if (Temp->second.ScanOrder_StoredDisplayedInverted)
+        {
+            if (Retrieve(Stream_Video, 0, Video_ScanOrder)==__T("BFF"))
+                Fill(Stream_Video, 0, Video_ScanOrder, "TFF", Unlimited, true, true);
+            else if (Retrieve(Stream_Video, 0, Video_ScanOrder)==__T("TFF"))
+                Fill(Stream_Video, 0, Video_ScanOrder, "BFF", Unlimited, true, true);
+            Fill(Stream_Video, 0, Video_ScanOrder_StoredDisplayedInverted, "Yes");
+        }
+
         //External file name specific
         if (Temp->second.MI && Temp->second.MI->Info)
         {
