194b3c50777388bcedf6e9f7733d663089d5fc9d
JeromeMartinez <jerome@mediaarea.net>
2014-02-05 11:16:31 +0000

    x STL: better support of non-English characters
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@6084 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/File__Analyze.h
+++ Source/MediaInfo/File__Analyze.h
@@ -657,8 +657,10 @@ public :
     //***************************************************************************
 
     void Get_Local  (int64u Bytes, Ztring      &Info, const char* Name);
+    void Get_ISO_6937_2(int64u Bytes, Ztring   &Info, const char* Name);
     void Get_ISO_8859_1(int64u Bytes, Ztring   &Info, const char* Name);
     void Get_ISO_8859_2(int64u Bytes, Ztring   &Info, const char* Name);
+    void Get_ISO_8859_5(int64u Bytes, Ztring   &Info, const char* Name);
     void Get_String (int64u Bytes, std::string &Info, const char* Name);
     void Get_UTF8   (int64u Bytes, Ztring      &Info, const char* Name);
     void Get_UTF16  (int64u Bytes, Ztring      &Info, const char* Name);
@@ -667,11 +669,13 @@ public :
     void Peek_Local (int64u Bytes, Ztring      &Info);
     void Peek_String(int64u Bytes, std::string &Info);
     void Skip_Local (int64u Bytes,                    const char* Name);
+    void Skip_ISO_6937_2(int64u Bytes,                const char* Name);
     void Skip_String(int64u Bytes,                    const char* Name);
     void Skip_UTF8  (int64u Bytes,                    const char* Name);
     void Skip_UTF16B(int64u Bytes,                    const char* Name);
     void Skip_UTF16L(int64u Bytes,                    const char* Name);
     #define Info_Local(_BYTES, _INFO, _NAME)  Ztring _INFO; Get_Local (_BYTES, _INFO, _NAME)
+    #define Info_ISO_6937_2(_BYTES, _INFO, _NAME)  Ztring _INFO; Get_ISO_6937_2 (_BYTES, _INFO, _NAME)
     #define Info_UTF8(_BYTES, _INFO, _NAME)   Ztring _INFO; Get_UTF8  (_BYTES, _INFO, _NAME)
     #define Info_UTF16B(_BYTES, _INFO, _NAME) Ztring _INFO; Get_UTF16B(_BYTES, _INFO, _NAME)
     #define Info_UTF16L(_BYTES, _INFO, _NAME) Ztring _INFO; Get_UTF16L(_BYTES, _INFO, _NAME)
