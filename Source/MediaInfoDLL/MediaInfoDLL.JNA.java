4724ebcedc534437d824a7406ddc8e96bc4df6c7
JeromeMartinez <jerome@mediaarea.net>
2014-02-21 15:58:30 +0000

    x Typos
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@6114 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfoDLL/MediaInfoDLL.JNA.java
+++ Source/MediaInfoDLL/MediaInfoDLL.JNA.java
@@ -84,11 +84,6 @@ class MediaInfo
                 @Override
                 public String getFunctionName(NativeLibrary lib, Method method)
                 {
-                    /*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license that can
- *  be found in the License.html file in the root of the source tree.
- */
                     return "MediaInfo_" + method.getName();
                 }
             }
