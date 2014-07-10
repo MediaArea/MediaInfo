1f865a66357197a3b062e508a540e43662fa51a2
JeromeMartinez <jerome@mediaarea.net>
2014-07-10 12:22:23 +0000

    x AVI: wrong ID during demux
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@6364 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Multiple/File_Riff_Elements.cpp
+++ Source/MediaInfo/Multiple/File_Riff_Elements.cpp
@@ -2407,7 +2407,6 @@ void File_Riff::AVI__movi_xxxx()
             Element_Code=((Element_Code_Old>>24)&0xF)*10+((Element_Code_Old>>16)&0xF);
             Frame_Count_NotParsedIncluded=Stream[Stream_ID].PacketPos;
             FrameInfo.DTS=Frame_Count_NotParsedIncluded*1000000000*Stream[Stream_ID].Scale/Stream[Stream_ID].Rate;
-            Element_Code=Stream_ID;
             Demux(Buffer+Buffer_Offset, (size_t)Element_Size, ContentType_MainStream);
             Element_Code=Element_Code_Old;
             Frame_Count_NotParsedIncluded=(int64u)-1;
