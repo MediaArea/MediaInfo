785846c74d0f30a1933e9175d6b9d9e17712e84f
JeromeMartinez <jerome@mediaarea.net>
2014-09-27 20:02:04 +0000

    x MPEG-TS: was not well configuring IBI for CSV input
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfoLib/trunk@6462 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Source/MediaInfo/Multiple/File_MpegTs.cpp
+++ Source/MediaInfo/Multiple/File_MpegTs.cpp
@@ -1962,12 +1962,13 @@ size_t File_MpegTs::Read_Buffer_Seek (size_t Method, int64u Value, int64u ID)
                 MI.Option(__T("ParseSpeed"), __T("0"));
                 MI.Option(__T("Demux"), Ztring());
                 Ztring File_Names=Config->File_Names.Read();
+                Config->File_Names.Separator_Set(0, ",");
                 MI.Option(__T("File_FileNameFormat"), __T("CSV"));
                 size_t MiOpenResult=MI.Open(File_Names);
                 MI.Option(__T("ParseSpeed"), ParseSpeed_Save); //This is a global value, need to reset it. TODO: local value
                 MI.Option(__T("Demux"), Demux_Save); //This is a global value, need to reset it. TODO: local value
                 if (!MiOpenResult)
-                    return 0;
+                    return -1;
                 for (ibi::streams::iterator IbiStream_Temp=((File_MpegTs*)MI.Info)->Ibi.Streams.begin(); IbiStream_Temp!=((File_MpegTs*)MI.Info)->Ibi.Streams.end(); ++IbiStream_Temp)
                 {
                     if (Ibi.Streams[IbiStream_Temp->first]==NULL)
