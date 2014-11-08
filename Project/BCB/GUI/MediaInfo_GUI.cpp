cb7255bbb6318ed0e5462247523184e27e368418
JeromeMartinez <jerome@mediaarea.net>
2014-11-08 15:46:51 +0000

    + BCB: migrating to XE2
    git-svn-id: svn://svn.code.sf.net/p/mediainfo/code/MediaInfo/trunk@6534 45c5ae6f-87cc-4fd0-92ee-e4f023fd80da

--- Project/BCB/GUI/MediaInfo_GUI.cpp
+++ Project/BCB/GUI/MediaInfo_GUI.cpp
@@ -2,41 +2,75 @@
 
 #include <vcl.h>
 #pragma hdrstop
+#include <tchar.h>
 //---------------------------------------------------------------------------
-USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Sheet.cpp", Preferences_SheetF);
-USEFORM("..\..\..\Source\GUI\VCL\GUI_Web.cpp", WebF);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Language.cpp", Preferences_LanguageF);
 USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Output.cpp", Preferences_OutputF);
-USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Custom.cpp", Preferences_CustomF);
-USEFORM("..\..\..\Source\GUI\VCL\GUI_Main.cpp", MainF);
-USEFORM("..\..\..\Source\GUI\VCL\GUI_Export.cpp", ExportF);
 USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences.cpp", PreferencesF);
+USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Custom.cpp", Preferences_CustomF);
+USEFORM("..\..\..\Source\GUI\VCL\GUI_Preferences_Sheet.cpp", Preferences_SheetF);
+USEFORM("..\..\..\Source\GUI\VCL\GUI_Web.cpp", WebF);
+USEFORM("..\..\..\Source\GUI\VCL\GUI_Export.cpp", ExportF);
+USEFORM("..\..\..\Source\GUI\VCL\GUI_Main.cpp", MainF);
 USEFORM("..\..\..\Source\GUI\VCL\GUI_About.cpp", AboutF);
 //---------------------------------------------------------------------------
-WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+WINAPI _tWinMain(HINSTANCE, HINSTANCE, LPTSTR, int)
 {
-    try
-    {
-        Application->Initialize();
-        Application->Title = "MediaInfo";
-        Application->CreateForm(__classid(TMainF), &MainF);
-        Application->Run();
-    }
-    catch (Exception &exception)
-    {
-        Application->ShowException(&exception);
-    }
-    catch (...)
-    {
-        try
-        {
-            throw Exception("");
-        }
-        catch (Exception &exception)
-        {
-            Application->ShowException(&exception);
-        }
-    }
-    return 0;
+	try
+	{
+		Application->Initialize();
+		Application->MainFormOnTaskBar = true;
+		Application->CreateForm(__classid(TMainF), &MainF);
+		Application->Run();
+	}
+	catch (Exception &exception)
+	{
+		Application->ShowException(&exception);
+	}
+	catch (...)
+	{
+		try
+		{
+			throw Exception("");
+		}
+		catch (Exception &exception)
+		{
+			Application->ShowException(&exception);
+		}
+	}
+	return 0;
 }
 //---------------------------------------------------------------------------
