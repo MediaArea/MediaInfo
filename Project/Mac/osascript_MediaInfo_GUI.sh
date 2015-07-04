c1cade47f049e36071142ef6491175035c58410b
Almin <almin@almin.tf>
2015-07-04 23:38:33 +0200

    Factorisation of mkdmg scripts

---
+++ Project/Mac/osascript_MediaInfo_GUI.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+osascript_Function () {
+	echo '
+		tell application "Finder"
+			tell disk "'${APPNAME}'"
+				open
+				set current view of container window to icon view
+				set toolbar visible of container window to false
+				set the bounds of container window to {400, 100, 950, 600}
+				set viewOptions to the icon view options of container window
+				set arrangement of viewOptions to not arranged
+				set icon size of viewOptions to 72
+				set background picture of viewOptions to file ".background:Logo_White.icns"
+				set position of item "'${APPNAME}'.app" of container window to {125, 175}
+				set position of item "Applications" of container window to {275, 200}
+				set position of item "License.html" of container window to {450, 175}
+				set position of item "History.txt" of container window to {450, 275}
+				close
+			end tell
+		end tell
+	' | osascript
+}
