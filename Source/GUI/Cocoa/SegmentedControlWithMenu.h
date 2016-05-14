a0a19ea0baa2fcd0ecd5e24521920092922942a4
Max Pozdeev <maxpozdeev@gmail.com>
2016-05-14 13:11:53 +0300

    Cocoa: add more views - Standards

---
+++ Source/GUI/Cocoa/SegmentedControlWithMenu.h
@@ -0,0 +1,13 @@
+//
+//  SegmentedControlWithMenu.h
+//  MediaInfo
+//
+//  Created by Max Pozdeev on 13.05.16.
+//  Copyright © 2016 MediaArea.net. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SegmentedControlWithMenu : NSSegmentedControl
+
+@end
