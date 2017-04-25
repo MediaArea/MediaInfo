e5bb49a81e44e61bb9e580c940893c7591d0fcd7
Maxime Gervais <gervais.maxime@gmail.com>
2017-04-25 14:13:19 +0200

    Add EBUCore 1.8 outputs to Cocoa GUI
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Source/GUI/Cocoa/MyWindowController.h
+++ Source/GUI/Cocoa/MyWindowController.h
@@ -10,7 +10,7 @@
 #import "oMediaInfoList.h"
 #import "HyperlinkButton.h"
 
-typedef enum { Kind_Text, Kind_XML, Kind_PBCore, Kind_PBCore2, Kind_reVTMD, Kind_MPEG7, Kind_EBUCore_1_5, Kind_EBUCore_1_6, Kind_FIMS_1_1, Kind_FIMS_1_2, Kind_FIMS_1_3} ViewMenu_Kind;
+typedef enum { Kind_Text, Kind_XML, Kind_PBCore, Kind_PBCore2, Kind_reVTMD, Kind_MPEG7, Kind_EBUCore_1_5, Kind_EBUCore_1_6, Kind_EBUCore_1_8_ps, Kind_EBUCore_1_8_sp, Kind_FIMS_1_1, Kind_FIMS_1_2, Kind_FIMS_1_3} ViewMenu_Kind;
 
 @interface MyWindowController : NSWindowController {
 
@@ -51,6 +51,8 @@ typedef enum { Kind_Text, Kind_XML, Kind_PBCore, Kind_PBCore2, Kind_reVTMD, Kind
 -(IBAction)selectViewPBCore2:(id)sender;
 -(IBAction)selectViewEBUCore15:(id)sender;
 -(IBAction)selectViewEBUCore16:(id)sender;
+-(IBAction)selectViewEBUCore18_ps:(id)sender;
+-(IBAction)selectViewEBUCore18_sp:(id)sender;
 -(IBAction)selectViewFIMS11:(id)sender;
 -(IBAction)selectViewFIMS12:(id)sender;
 -(IBAction)selectViewFIMS13:(id)sender;
