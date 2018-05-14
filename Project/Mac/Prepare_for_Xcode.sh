d7e279c5822ec8394606f9f2ac1f2620c1b33b78
Maxime Gervais <gervais.maxime@gmail.com>
2018-05-14 09:53:19 +0200

    Build only 64 bit binary for macStore
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/Mac/Prepare_for_Xcode.sh
+++ Project/Mac/Prepare_for_Xcode.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-cp ../dylib_for_xcode/MediaInfoLib/libmediainfo.dylib MediaInfo/Source/GUI/Cocoa ;
-cd MediaInfo/Source/GUI/Cocoa ;
+lipo -extract "x86_64" ../dylib_for_xcode/MediaInfoLib/libmediainfo.dylib -o MediaInfo/Source/GUI/Cocoa/libmediainfo.dylib
+cd MediaInfo/Source/GUI/Cocoa
 install_name_tool -id @executable_path/../Resources/libmediainfo.dylib libmediainfo.dylib ;
 
 cd _i18n
