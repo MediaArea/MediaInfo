0029e2f3f2019886672fa2ea4506fa7d7fc5ba87
Maxime Gervais <gervais.maxime@gmail.com>
2018-07-13 12:06:38 +0200

    Update xCode project
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/Mac/Prepare_for_Xcode.sh
+++ Project/Mac/Prepare_for_Xcode.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-lipo -extract "x86_64" ../dylib_for_xcode/MediaInfoLib/libmediainfo.dylib -o MediaInfo/Source/GUI/Cocoa/libmediainfo.dylib
+lipo -thin "x86_64" ../dylib_for_xcode/MediaInfoLib/libmediainfo.dylib -o MediaInfo/Source/GUI/Cocoa/libmediainfo.dylib
 cd MediaInfo/Source/GUI/Cocoa
 install_name_tool -id @executable_path/../Resources/libmediainfo.dylib libmediainfo.dylib ;
 
