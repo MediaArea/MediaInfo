86979aff52b702c7aecae2623e8bd252b73ba3fb
Maxime Gervais <gervais.maxime@gmail.com>
2020-12-29 20:02:07 +0100

    Add arm64 architucture for macOS
    + Set minimal macOS version to 10.9
    
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Project/Mac/BR_extension_GUI.sh
+++ Project/Mac/BR_extension_GUI.sh
@@ -9,6 +9,4 @@ cd ../../../../MediaInfo/Project/GNU/GUI
 ./autogen.sh
 cd ../../../..
 
-# Because wx doesn't compile in 32 bits
-#./GUI_Compile.sh --with-wx-static --enable-arch-x86_64 --enable-arch-i386
-./GUI_Compile.sh --with-wx-static
+./GUI_Compile.sh --with-wx-static --enable-arch-x86_64 --enable-arch-arm64
