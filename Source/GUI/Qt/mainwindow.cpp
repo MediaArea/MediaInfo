3dc1c266cee184bf27a2c2cc506b6b5db8ebc842
Jenkins CI <ci@mediaarea.net>
2024-06-27 13:43:04 +0000

    Preparing v24.06

--- Source/GUI/Qt/mainwindow.cpp
+++ Source/GUI/Qt/mainwindow.cpp
@@ -46,7 +46,7 @@ using namespace ZenLib;
 #define QString2wstring(_DATA) \
     Ztring().From_UTF8(_DATA.toUtf8())
 
-#define VERSION "24.05"
+#define VERSION "24.06"
 #if defined(_WIN32) && defined(WINAPI_FAMILY) && (WINAPI_FAMILY==WINAPI_FAMILY_APP) //UWP Application
 #include <QtPlatformHeaders/QWindowsWindowFunctions>
 #include <ZenLib/Os_Utils.h>
