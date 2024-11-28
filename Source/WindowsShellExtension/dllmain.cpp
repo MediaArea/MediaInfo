60b288a35403b0f000e9669c12ada936ae86fe45
cjee21 <77721854+cjee21@users.noreply.github.com>
2024-11-28 22:26:20 +0800

    Add Windows shell extension for File Explorer context menu

---
+++ Source/WindowsShellExtension/dllmain.cpp
@@ -0,0 +1,203 @@
+/*  Copyright (c) MediaArea.net SARL. All Rights Reserved.
+ *
+ *  Use of this source code is governed by a BSD-style license that can
+ *  be found in the License.html file in the root of the source tree.
+ */
+
+// dllmain.cpp : Defines the entry point for the DLL application.
+
+// IExplorerCommand File Explorer shell extension for context menu entry
+
+#include "pch.h"
+
+#pragma warning(disable : 4324)
+
+using Microsoft::WRL::ClassicCom;
+using Microsoft::WRL::ComPtr;
+using Microsoft::WRL::InhibitRoOriginateError;
+using Microsoft::WRL::Module;
+using Microsoft::WRL::ModuleType;
+using Microsoft::WRL::RuntimeClass;
+using Microsoft::WRL::RuntimeClassFlags;
+
+constexpr const wchar_t* menu_entry_title = L"MediaInfo";
+constexpr const wchar_t* exe_filename = L"MediaInfo.exe";
+
+BOOL APIENTRY DllMain(_In_ HMODULE hModule, _In_ DWORD  ul_reason_for_call, _In_opt_ LPVOID lpReserved) {
+    UNREFERENCED_PARAMETER(hModule);
+    UNREFERENCED_PARAMETER(lpReserved);
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+namespace {
+    // Extracted from
+    // https://source.chromium.org/chromium/chromium/src/+/main:base/command_line.cc;l=109-159
+
+    std::wstring QuoteForCommandLineArg(_In_ const std::wstring& arg) {
+        // We follow the quoting rules of CommandLineToArgvW.
+        // http://msdn.microsoft.com/en-us/library/17w5ykft.aspx
+        std::wstring quotable_chars(L" \\\"");
+        if (arg.find_first_of(quotable_chars) == std::wstring::npos) {
+            // No quoting necessary.
+            return arg;
+        }
+
+        std::wstring out;
+        out.push_back('"');
+        for (size_t i = 0; i < arg.size(); ++i) {
+            if (arg[i] == '\\') {
+                // Find the extent of this run of backslashes.
+                size_t start = i, end = start + 1;
+                for (; end < arg.size() && arg[end] == '\\'; ++end) {}
+                size_t backslash_count = end - start;
+
+                // Backslashes are escapes only if the run is followed by a double quote.
+                // Since we also will end the string with a double quote, we escape for
+                // either a double quote or the end of the string.
+                if (end == arg.size() || arg[end] == '"') {
+                    // To quote, we need to output 2x as many backslashes.
+                    backslash_count *= 2;
+                }
+                for (size_t j = 0; j < backslash_count; ++j)
+                    out.push_back('\\');
+
+                // Advance i to one before the end to balance i++ in loop.
+                i = end - 1;
+            }
+            else if (arg[i] == '"') {
+                out.push_back('\\');
+                out.push_back('"');
+            }
+            else {
+                out.push_back(arg[i]);
+            }
+        }
+        out.push_back('"');
+
+        return out;
+    }
+
+}
+
+class __declspec(uuid("20669675-b281-4c4f-94fb-cb6fd3995545")) ExplorerCommandHandler final : public RuntimeClass<RuntimeClassFlags<ClassicCom | InhibitRoOriginateError>, IExplorerCommand> {
+public:
+    // IExplorerCommand implementation:
+
+    IFACEMETHODIMP GetTitle(_In_opt_ IShellItemArray* items, _Outptr_ PWSTR* name) {
+        // Provide name for display in File Explorer context menu entry
+        UNREFERENCED_PARAMETER(items);
+        return SHStrDupW(menu_entry_title, name);
+    }
+
+    IFACEMETHODIMP GetIcon(_In_opt_ IShellItemArray* items, _Outptr_ PWSTR* icon) {
+        // Provide icon for display in File Explorer context menu entry
+        // Get path to application exe and use it as source for icon
+        UNREFERENCED_PARAMETER(items);
+        std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
+        module_path = module_path.remove_filename();
+        module_path /= exe_filename;
+        return SHStrDupW(module_path.c_str(), icon);
+    }
+
+    IFACEMETHODIMP GetToolTip(_In_opt_ IShellItemArray* items, _Outptr_ PWSTR* infoTip) {
+        UNREFERENCED_PARAMETER(items);
+        *infoTip = nullptr;
+        return E_NOTIMPL;
+    }
+
+    IFACEMETHODIMP GetCanonicalName(_Out_ GUID* guidCommandName) {
+        *guidCommandName = GUID_NULL;
+        return S_OK;
+    }
+
+    IFACEMETHODIMP GetState(_In_opt_ IShellItemArray* items, _In_ BOOL okToBeSlow, _Out_ EXPCMDSTATE* cmdState) {
+        UNREFERENCED_PARAMETER(items);
+        UNREFERENCED_PARAMETER(okToBeSlow);
+        *cmdState = ECS_ENABLED;
+        return S_OK;
+    }
+
+    IFACEMETHODIMP GetFlags(_Out_ EXPCMDFLAGS* flags) {
+        *flags = ECF_DEFAULT;
+        return S_OK;
+    }
+
+    IFACEMETHODIMP EnumSubCommands(_Outptr_ IEnumExplorerCommand** enumCommands) {
+        *enumCommands = nullptr;
+        return E_NOTIMPL;
+    }
+
+    IFACEMETHODIMP Invoke(_In_opt_ IShellItemArray* items, _In_opt_ IBindCtx* bindCtx) {
+        // Process items passed by File Explorer when context menu entry is invoked
+        UNREFERENCED_PARAMETER(bindCtx);
+        if (items) {
+            // Return if no items
+            DWORD count;
+            RETURN_IF_FAILED(items->GetCount(&count));
+
+            // Get path to application exe
+            std::filesystem::path module_path{ wil::GetModuleFileNameW<std::wstring>(wil::GetModuleInstanceHandle()) };
+            module_path = module_path.remove_filename();
+            module_path /= exe_filename;
+
+            // Prepare cmd line string to invoke application ("path\to\application.exe" "path\to\firstitem" "path\to\nextitem" ...)
+            auto command = wil::str_printf<std::wstring>(LR"-("%s")-", module_path.c_str()); // Path to application.exe
+            // Add multiple selected files/folders to cmd line as parameters
+            for (DWORD i = 0; i < count; ++i) {
+                ComPtr<IShellItem> item;
+                auto result = items->GetItemAt(i, &item);
+                if (SUCCEEDED(result)) {
+                    wil::unique_cotaskmem_string path;
+                    result = item->GetDisplayName(SIGDN_FILESYSPATH, &path);
+                    if (SUCCEEDED(result)) {
+                        // Append the item path to the existing command, adding quotes and escapes as needed
+                        command = wil::str_printf<std::wstring>(LR"-(%s %s)-", command.c_str(), QuoteForCommandLineArg(path.get()).c_str());
+                    }
+                }
+            }
+
+            // Invoke application using CreateProcess with the command string prepared above
+            wil::unique_process_information process_info;
+            STARTUPINFOW startup_info = { sizeof(startup_info) };
+            RETURN_IF_WIN32_BOOL_FALSE(CreateProcessW(
+                nullptr,
+                command.data(),
+                nullptr /* lpProcessAttributes */,
+                nullptr /* lpThreadAttributes */,
+                false /* bInheritHandles */,
+                CREATE_NO_WINDOW,
+                nullptr,
+                nullptr,
+                &startup_info,
+                &process_info));
+        }
+        return S_OK;
+    }
+};
+
+CoCreatableClass(ExplorerCommandHandler)
+CoCreatableClassWrlCreatorMapInclude(ExplorerCommandHandler)
+
+_Check_return_
+STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv) {
+    if (ppv == nullptr)
+        return E_POINTER;
+    *ppv = nullptr;
+    return Module<ModuleType::InProc>::GetModule().GetClassObject(rclsid, riid, ppv);
+}
+
+__control_entrypoint(DllExport)
+STDAPI DllCanUnloadNow(void) {
+    return Module<ModuleType::InProc>::GetModule().GetObjectCount() == 0 ? S_OK : S_FALSE;
+}
+
+STDAPI DllGetActivationFactory(_In_ HSTRING activatableClassId, _Out_ IActivationFactory** factory) {
+    return Module<ModuleType::InProc>::GetModule().GetActivationFactory(activatableClassId, factory);
+}
---
+++ Source/WindowsShellExtension/pch.h
@@ -0,0 +1,30 @@
+// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+#include "framework.h"
+
+#include <filesystem>
+#include <string>
+
+#include <shlobj_core.h>
+#include <shlwapi.h>
+#pragma comment(lib, "shlwapi.lib")
+
+// Windows Runtime C++ Template Library (WRL)
+#include <wrl/client.h>
+#include <wrl/implements.h>
+#include <wrl/module.h>
+#pragma comment(lib, "runtimeobject.lib")
+
+// Windows Implementation Libraries (WIL)
+#include <wil/stl.h>
+#include <wil/win32_helpers.h>
+
+#endif //PCH_H
