75e448bf3f787f430a7218626cf892b2e3f4aea2
cjee21 <77721854+cjee21@users.noreply.github.com>
2025-11-15 00:49:20 +0800

    Windows GUI: Update uninstaller signing timestamp server

--- Source/Install/sign.cmd
+++ Source/Install/sign.cmd
@@ -1,5 +1,5 @@
 set /P CodeSigningCertificatePass= < %USERPROFILE%\CodeSigningCertificate.pass
 if "%NOSIGN%"=="" (
-    signtool.exe sign /f %USERPROFILE%\CodeSigningCertificate.p12 /p %CodeSigningCertificatePass% /fd sha256 /v /tr http://ts.ssl.com /td sha256 /d %2 /du http://mediaarea.net %1
+    signtool.exe sign /f %USERPROFILE%\CodeSigningCertificate.p12 /p %CodeSigningCertificatePass% /fd sha256 /v /tr http://timestamp.acs.microsoft.com /td sha256 /d %2 /du http://mediaarea.net %1
 )
 set CodeSigningCertificatePass=
