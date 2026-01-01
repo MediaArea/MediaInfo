c1e0bfb0396a02a1a040d15283a4fea8559e4819
cjee21 <77721854+cjee21@users.noreply.github.com>
2026-01-02 01:57:56 +0800

    Android TV GUI: Enable minify and shrink

--- Source/GUI/Android/tvapp/build.gradle.kts
+++ Source/GUI/Android/tvapp/build.gradle.kts
@@ -59,7 +59,8 @@ android {
     }
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
--- Source/GUI/Android/tvapp/proguard-rules.pro
+++ Source/GUI/Android/tvapp/proguard-rules.pro
@@ -19,3 +19,8 @@
 # If you keep the line number information, uncomment this to
 # hide the original source file name.
 #-renamesourcefileattribute SourceFile
+
+# MediaInfo
+-keepclassmembers class net.mediaarea.mediainfo.MediaInfo {
+    *;
+}
