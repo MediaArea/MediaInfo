549f5e776850ece6fd29ed238f0fe60e68946641
Maxime Gervais <gervais.maxime@gmail.com>
2026-01-28 22:54:36 +0100

    Windows GUI: Export uninstaller for signing
    Signed-off-by: Maxime Gervais <gervais.maxime@gmail.com>

--- Source/Install/sign.cmd
+++
@@ -1,5 +0,0 @@
-set /P CodeSigningCertificatePass= < %USERPROFILE%\CodeSigningCertificate.pass
-if "%NOSIGN%"=="" (
-    signtool.exe sign /f %USERPROFILE%\CodeSigningCertificate.p12 /p %CodeSigningCertificatePass% /fd sha256 /v /tr http://timestamp.acs.microsoft.com /td sha256 /d %2 /du http://mediaarea.net %1
-)
-set CodeSigningCertificatePass=
